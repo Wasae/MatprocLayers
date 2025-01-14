435482d4ffe279d9b5fd8502415999ca39084da8
Abdul Wasae Momin <mominwasae8@gmail.com>
2025-01-14 13:18:39 +0530

    Setting Up DBContext Project
    Setting Up DBContext Project to Connect to Sql Server, Adding references to necessary packages, making space for connection strings. prototype for adding relationships between entities

---
+++ DbContext/EFContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace DBContext
+{
+    public class EFContext:DbContext
+    {
+        private string _connectionstring;
+        public EFContext(string connectcionstring)
+        {
+            connectcionstring = _connectionstring;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder options)
+        {
+            // connect to sql server with connection string from app settings
+            options.UseSqlServer(_connectionstring);
+        }
+
+        protected override void OnModelCreating(Microsoft.EntityFrameworkCore.ModelBuilder modelBuilder)
+        {
+            //Define relationship between entities with has/with kind of relationship
+            //one Company has many employees
+            // modelBuilder.Entity<Company>()
+            //     .HasMany(c => c.Employees)
+            //     .WithOne(e => e.Company);
+            //many employees has one Company
+            // modelBuilder.Entity<Employee>()
+            //     .HasOne(e => e.Company)
+            //     .WithMany(c => c.Employees);
+        }
+
+
+        //Add Entites Below
+    }
+}
